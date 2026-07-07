f241c1b67aa91e69988742d0d64ef84cafdcf258
Rishi Bhardwaj <rishi2208bhardwaj@gmail.com>
2026-07-07 20:20:52 +0530

    Delete SentencePiece Word Splitting.py

--- SentencePiece Word Splitting.py
+++
@@ -1,8 +0,0 @@
-# Pull in the sentencepiece package helper as sp to handle our text segmentation
-import sentencepiece as sp
-
-# Set up the short sample string that we want to chop into subword tokens
-sentence = "Artificial Intelligence"
-
-# Fire up the encoder method to split our text into clean, readable string fragments
-print(sp.encode(sentence, out_type=str))
